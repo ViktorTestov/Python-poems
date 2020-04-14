d20f25b6d82c5e60f97081ec66aa5fa33531ad61
ViktorTestov <61625464+ViktorTestov@users.noreply.github.com>
2020-04-14 13:25:14 +0300

    Update poems_parser.py
    Test Task2
    Added functionality for parsing the following arguments:
    --sort-poems (name, years)
    --sort-order (asc, desc)
    --display-novel 
    Made the write to file optional  (only if –output_file argument is present)

--- poems_parser.py
+++ poems_parser.py
@@ -1,67 +1,92 @@
-# Create file requirements.txt and put there all external modules (BeautifulSoup)
-# https://pip.readthedocs.io/en/1.1/requirements.html
-
-# Add .gitignore file to the repo, and configure it to ignore IDE specific folder ".idea"
-# https://git-scm.com/docs/gitignore
-
-# Please read how to create pull requests, it will be much more easier to review next changes
-# https://help.github.com/en/desktop/contributing-to-projects/creating-a-pull-request
 import urllib.request
 from bs4 import BeautifulSoup
 import os
 import argparse
-import codecs
+import re
+import operator
 
 
-# Open and read url
-# This is one line function and you use it only once in your code, it's not necessary.
+# Open resource
 def get_resource(url):
     resource = urllib.request.urlopen(url)
     return resource.read()
 
 
-# Parse html
-def parse_page(html):
+# parser
+def parse_page(html, url):
     res = []
     soup = BeautifulSoup(html, 'html.parser')
     table = soup.find('table', width="99%", border="0", cellspacing="2", cellpadding="2")
     for row in table.find_all('td', valign='top'):
-        res.append(row.find('a', class_='linki2'))
+        poem_name = row.find('a', class_='linki2').text
+        poem_ur = url + row.find('a', class_='linki2')['href']
+        try:
+            poem_year = row.find('span').text
+        except AttributeError:
+            continue
+        poem_year = re.findall('[0-9]{4}', poem_year)
+
+        res.append([poem_name, poem_year[0], poem_ur])
     return res
 
 
+# парсить посилання на вірш
+def parse_poem_by_url(url):
+    poem_page = get_resource(url)
+    soup = BeautifulSoup(poem_page, 'html.parser')
+    poem_header = soup.find('span', class_="text-head2").text
+    poem_text = soup.find('p', align='left').text
+    poem = (poem_header + '\n' + poem_text + '\n')
+    return poem
+
+
+def print_whole_poem(poem_url):
+    poem = parse_poem_by_url(poem_url)
+    print(poem)
+
+
 def main():
-    # parse the arguments
-    parser = argparse.ArgumentParser(description='My arsg parser')  # You can add better description :)
-    parser.add_argument('--output_file', action="store", type=str)
-    args = parser.parse_args()
     # parse the poems titles
-    poem = parse_page(get_resource('http://taras-shevchenko.in.ua/virshi-shevchenka.html'))
-    # create folder
-    path = os.path.dirname(os.path.abspath(args.output_file))
-    if not os.path.exists(path):   # in python3.7+ you can avoid this check, just:
-        os.makedirs(path)          # os.makedirs(path, exist_ok=True)
-
-    # There is a way to make it much more simple
-    # with open(args.output_file, "w") as f:
-    #     for p in poem:
-    #         f.write(p.string.strip() + '\n')
-
-    # open file
-    file_to_write = codecs.open(args.output_file, "w", "utf-8")
-    # write to file
-    for p in poem:
-        file_to_write.write(p.string.strip() + '\n')
-    # close the file
-    file_to_write.close()
-
-    # you don't need read this data from file, just:
-    # print('Amount of poems =', len(poem))
-
-    with codecs.open(args.output_file, "r", "utf-8") as file:
-        print('Amount of poems =', len(file.readlines()))
-
-    print("Absolute path = ", path)
+    url = 'http://taras-shevchenko.in.ua/'
+    poem = parse_page(get_resource(url + 'virshi-shevchenka.html'), url)
+    # parse the arguments
+    parser = argparse.ArgumentParser(description='My arsg parser')
+    parser.add_argument('--output_file', action="store", type=str)
+    parser.add_argument('--sort_poems', '-name', '-years', action='store', nargs=1)
+    parser.add_argument('--sort_order', '-asc', '-desc', action='store', nargs=1)
+    parser.add_argument('--display_novel', action='store')
+    args = parser.parse_args()
+    reverse_sort_order = False
+    if args.sort_order and args.sort_order[0] == 'desc':
+        reverse_sort_order = True
+    if args.output_file:
+        # create folder
+        path = os.path.dirname(os.path.abspath(args.output_file))
+        if not os.path.exists(path):
+            os.makedirs(path)
+        # open file
+        with open(args.output_file, "w", encoding='utf-8') as f:
+            for p in poem:
+                f.write((p[0].strip() + ' ' + p[1].strip()) + '\n')
+        print('Count of poem =', len(poem))
+        print("Absolute path = ", path)
+    elif args.sort_poems and args.sort_poems[0] == 'name':
+        sorted_poems = sorted(poem, key=operator.itemgetter(0), reverse=reverse_sort_order)
+        for p in sorted_poems:
+            print(p[0].strip() + ' ' + p[1])
+    elif args.sort_poems and args.sort_poems[0] == 'years':
+        sorted_poems = sorted(poem, key=operator.itemgetter(1), reverse=reverse_sort_order)
+        for p in sorted_poems:
+            print(p[0].strip() + ' ' + p[1])
+    elif args.display_novel:
+        novel_names = args.display_novel.split('|')
+        for p in poem:
+            for n in novel_names:
+                if n.strip() == p[0]:
+                    print_whole_poem(p[2])
+    else:
+        for p in poem:
+            print(p[0].strip() + ' ' + p[1])
 
 
 if __name__ == '__main__':
